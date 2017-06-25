e682519eb8e4896b5ba9f9b3827277edf4746e09
Bruno Morgado <mstrk512@gmail.com>
2017-06-25 06:29:47 +0100

    merge master

--- src/containers/App/routes.js
+++ src/containers/App/routes.js
@@ -7,22 +7,18 @@ import NotFound from 'components/NotFound'
 import Login from 'containers/Auth/Login'
 import HomePage from 'containers/HomePage'
 import Filter from 'containers/Filter'
-import Protected from 'components/Protected'
 import AddProfile from 'containers/AddProfile'
 import Profile from 'containers/Profile'
 import ChatPage from 'containers/ChatPage'
-import Protected from 'components/Protected'
 
 export default () => (
   <Switch>
     <Route path='/' exact component={HomePage} />
     <Route path='/login' component={Login} />
-    <Route path='/filter' component={Filter} />
-    <Route path='/protected' component={requireAuth(Protected, 'user')} />
-    <Route path='/addprofile' component={AddProfile} />
-    <Route path='/profile' component={Profile} />
-    <Route path='/chat' component={ChatPage} />
-    <Route path='/protected' component={requireAuth(Protected, 'user')} />
+    <Route path='/filter' component={requireAuth(Filter, 'user')} />
+    <Route path='/addprofile' component={requireAuth(AddProfile, 'user')} />
+    <Route path='/profile' component={requireAuth(Profile, 'user')} />
+    <Route path='/chat' component={requireAuth(ChatPage, 'user')} />
     <Route component={NotFound} />
   </Switch>
 )
