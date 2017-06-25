e7931002369a0914f390477431d46712461de58c
a31859 <a31859@users.noreply.github.com>
2017-06-25 04:21:48 +0100

    add chat client

--- src/containers/App/routes.js
+++ src/containers/App/routes.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router'
 import requireAuth from 'containers/Auth'
 import Login from 'containers/Auth/Login'
 import HomePage from 'containers/HomePage'
+import ChatPage from 'containers/ChatPage'
 import Protected from 'components/Protected'
 import NotFound from 'components/NotFound'
 
@@ -12,6 +13,7 @@ export default () => (
   <Switch>
     <Route path='/' exact component={HomePage} />
     <Route path='/login' component={Login} />
+    <Route path='/chat' component={ChatPage} />
     <Route path='/protected' component={requireAuth(Protected, 'user')} />
     <Route component={NotFound} />
   </Switch>
