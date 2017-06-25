a759c8981f1f0c5007c6695d4269ff42d6213602
Cláudio Alves <claudio@cloudoki.com>
2017-06-25 01:36:14 +0100

    Duplicate route fix

--- src/containers/App/routes.js
+++ src/containers/App/routes.js
@@ -2,13 +2,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
 
-// import requireAuth from 'containers/Auth'
+import requireAuth from 'containers/Auth'
 import NotFound from 'components/NotFound'
 import Login from 'containers/Auth/Login'
 import HomePage from 'containers/HomePage'
 import Filter from 'containers/Filter'
 import Protected from 'components/Protected'
-import NotFound from '../NotFound'
 import AddProfile from 'containers/AddProfile'
 
 export default () => (
